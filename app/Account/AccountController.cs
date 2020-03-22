987f942986277905c3eeb16e3ea0983fe0c7edab
Konstantin Pelz <konstantin.pelz@online.de>
2020-03-22 13:10:16 +0100

    now sending mails is possible

--- app/Account/AccountController.cs
+++ app/Account/AccountController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace MyContribution.Backend
 {
@@ -12,10 +14,12 @@ namespace MyContribution.Backend
     public class AccountController : ControllerBase
     {
         private readonly DataContext ctx;
+        private readonly IConfiguration config;
 
-        public AccountController(DataContext ctx)
+        public AccountController(DataContext ctx, IConfiguration config)
         {
             this.ctx = ctx;
+            this.config = config;
         }
 
         [HttpGet("{key}")]
@@ -30,6 +34,7 @@ namespace MyContribution.Backend
             var (tlon, tlat) = API.GetLongLat("Unter den Linden, 1, Berlin");
             var (t2lon, t2lat) = API.GetLongLat("akjsgbfkjagshfjkahslkf");
             var test = API.GetName(tlon, tlat);
+            MessageFromInstitution("Konsti", "konstantin.buy@online.de", "Kreiskrankenhaus EBE", "Praetoriusbogen 25 <br> 85560 Ebersberg", "Hey Konstantin,<br>wir brauchen dich. Und zwar jetzt!");
             Account acc = new Account()
             {
                 Username = request.Username,
@@ -45,5 +50,53 @@ namespace MyContribution.Backend
             return Created("odata/Contacts", acc);
         }
 
+        public bool MessageFromInstitution(string name, string receiver, string nameInstitution, string contactInfo, string message) {
+            return SendMail("Die Zeit für dein Heldentum ist gekommen!", GetFormattedMessageHTML(name, nameInstitution, contactInfo, message),receiver);
+        }
+
+        private bool SendMail(string subject, string body, string receiver)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient(config.GetValue<string>("SMTP_HOST"));
+
+                mail.From = new MailAddress("noreply@corona-helden.net","Corona-Helden");
+                mail.To.Add(receiver);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+
+                SmtpServer.Port = Int16.Parse(config.GetValue<string>("SMTP_PORT"));
+                SmtpServer.Credentials = new System.Net.NetworkCredential(config.GetValue<string>("SMTP_USER"), config.GetValue<string>("SMTP_PASS"));
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.ToString());
+                return false;
+            }
+        }
+
+        private String GetFormattedMessageHTML(string name, string nameInstitution, string contactInfo, string message)
+        {
+            return "<!DOCTYPE html> " +
+                "<html xmlns=\"http://www.w3.org/1999/xhtml\">" +
+                "<head>" +
+                    "<title>Email</title>" +
+                "</head>" +
+                "<body style=\"font-family:'Century Gothic'\">" +
+                    "<h1 style=\"text-align:center;\"> Du bist ein Corona-Held und jetzt wirst du gebraucht!</h1>" +
+                    "<h2 style=\"font-size:14px;\">" +
+                        "Liebe(r) " + name + ", hier die Nachricht von: " + nameInstitution +
+                    "</h2>" +
+                    "<p>" + message + "</p>" +
+                    "<p><i>" + contactInfo + "</i></p>" +
+                "</body>" +
+                "</html>";
+        }
     }
 }
